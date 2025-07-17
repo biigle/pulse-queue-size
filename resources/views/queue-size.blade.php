e8395cf79defe59399a41aef632ce98a0d969bc0
Leane Schlundt <lhecht@techfak.de>
2025-07-17 10:03:53 +0200

    Add card view

---
+++ resources/views/queue-size.blade.php
@@ -0,0 +1,33 @@
+<x-pulse::card title="Queue Size" :cols="$cols">
+    <x-pulse::card-header name="Queue size"></x-pulse::card-header>
+    <x-pulse::scroll :expand="$expand" wire:poll.5s="">
+    @if($queues->isEmpty())
+        <x-pulse::no-results />
+    @else
+        <x-pulse::table>
+            <colgroup>
+                <col width="100%" />
+                <col width="100%" />
+            </colgroup>
+            <x-pulse::thead>
+                <tr>
+                    <x-pulse::th>Queue</x-pulse::th>
+                    <x-pulse::th>Size</x-pulse::th>
+                </tr>
+            </x-pulse::thead>
+            <tbody>
+                @foreach ($queues as $queue)
+                    <tr wire:key="{{ $queue->key }}-spacer" class="h-2 first:h-0"></tr>
+                    <tr wire:key="{{ $queue->key }}-row">
+                    <x-pulse::td class="max-w-[1px]">
+                    {!! $queue->key !!}
+                    </x-pulse::td>
+                    <x-pulse::td class="max-w-[1px]">
+                    {!! intval($queue->sum) !!}
+                    </x-pulse::td>
+                @endforeach
+            </tbody>
+        </x-pulse::table>
+    @endif
+    </x-pulse::scroll>
+</x-pulse::card>
