3a2706c157e1e0c3bf8c24f3f9b02bd43541f249
Leane Schlundt <lhecht@techfak.de>
2025-07-23 09:55:34 +0200

    Add service provider

---
+++ src/PulseQueueSizeCardServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Biigle\PulseQueueSizeCard;
+
+
+use Livewire\Livewire;
+use Biigle\PulseQueueSizeCard\Http\Livewire\QueueSize;
+
+use Illuminate\Support\ServiceProvider;
+
+class PulseQueueSizeCardServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Livewire::component('pulse-queue-size-card.queue-size', QueueSize::class);
+
+        $this->loadViewsFrom(__DIR__.'/../resources/views', "pulse-queue-size-card");
+
+    }
+}
