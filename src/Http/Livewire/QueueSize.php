552763bb69fd6a31c820a6034e02992e4db97ee5
Leane Schlundt <lhecht@techfak.de>
2025-07-23 09:27:44 +0200

    Remove log

--- src/Http/Livewire/QueueSize.php
+++ src/Http/Livewire/QueueSize.php
@@ -2,19 +2,13 @@
 
 namespace Biigle\PulseQueueSizeCard\Http\Livewire;
 
-use Laravel\Pulse\Facades\Pulse;
 use Laravel\Pulse\Livewire\Card;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\View;
-
 
 class QueueSize extends Card
 {
     public function render()
     {
         $queues = $this->aggregate('queue_size', 'sum');
-        Log::info($queues);
         return view('pulse-queue-size-card::queue-size', ['queues' => $queues]);
     }
 }
