a4ce07d61526fd448a2877bafccb30acff8f2e38
Leane Schlundt <lhecht@techfak.de>
2025-08-19 08:36:13 +0200

    Show chart for each queue

--- resources/views/queue-size.blade.php
+++ resources/views/queue-size.blade.php
@@ -1,129 +1,141 @@
 <x-pulse::card title="Queue Size" :cols="$cols">
-    <x-pulse::card-header 
-    name="Queue Size" 
-    x-bind:title="`Time: {{ number_format($time) }}ms; Run at: ${formatDate('{{ $runAt }}')};`"
-    details="past {{ $this->periodForHumans() }}"
-    >
+    <x-pulse::card-header name="Queue Size"
+        x-bind:title="`Time: {{ number_format($time) }}ms; Run at: ${formatDate('{{ $runAt }}')};`"
+        details="past {{ $this->periodForHumans() }}">
         <x-slot:icon>
-            <x-pulse::icons.queue-list/>
+            <x-pulse::icons.queue-list />
         </x-slot:icon>
     </x-pulse::card-header>
     <x-pulse::scroll :expand="$expand" wire:poll.5s="">
-    @if($queues->isEmpty())
-        <x-pulse::no-results />
-    @else
-        <div class="grid gap-3 mx-px mb-px">
-            @foreach ($queues as $queue => $readings)
-                <div wire:key="{{ $queue }}">
-                    <h3 class="font-bold text-gray-700 dark:text-gray-300">
-                        {!! $queue !!}
-                    </h3>
+        @if ($queues->isEmpty())
+            <x-pulse::no-results />
+        @else
+            <div class="grid gap-3 mx-px mb-px">
+                @foreach ($queues as $queue => $readings)
+                    @php
+                        $last = $readings->flatten()->last();
+                        $queueInfo = explode(':', $queue);
+                    @endphp
+                    <div wire:key="{{ $queue }}">
+                        <div class="flex items-center gap-2">
+                            <h3 class="font-bold text-gray-700 dark:text-gray-300">
+                                {!! $queueInfo[1] !!}
+                            </h3>
+                            <h3 class="text-gray-600">
+                                {!! '(' . $queueInfo[0] . ')' !!}
+                            </h3>
+                        </div>
 
-                    <div class="mt-3 relative">
-                        {{-- <div class="absolute -left-px -top-2 max-w-fit h-4 flex items-center px-1 text-xs leading-none text-white font-bold bg-purple-500 rounded after:[--triangle-size:4px] after:border-l-purple-500 after:absolute after:right-[calc(-1*var(--triangle-size))] after:top-[calc(50%-var(--triangle-size))] after:border-t-[length:var(--triangle-size)] after:border-b-[length:var(--triangle-size)] after:border-l-[length:var(--triangle-size)] after:border-transparent">
-                            {{ number_format($readings->sum) }}
-                        </div> --}}
-
-                        <div
-                            wire:ignore
-                            class="h-14"
-                            x-data="queueSizeChart({
+                        <div class="mt-3 relative">
+                            <div
+                                class="absolute -left-px -top-2 max-w-fit h-4 flex items-center px-1 text-xs leading-none text-white font-bold bg-purple-500 rounded after:[--triangle-size:4px] after:border-l-purple-500 after:absolute after:right-[calc(-1*var(--triangle-size))] after:top-[calc(50%-var(--triangle-size))] after:border-t-[length:var(--triangle-size)] after:border-b-[length:var(--triangle-size)] after:border-l-[length:var(--triangle-size)] after:border-transparent">
+                                {{ number_format($last) }}
+                            </div>
+                            <div wire:ignore class="h-14" x-data="queueSizeChart({
                                 queue: '{{ $queue }}',
                                 readings: @js($readings),
-                            })"
-                        >
-                            <canvas x-ref="canvas" class="ring-1 ring-gray-900/5 dark:ring-gray-100/10 bg-gray-50 dark:bg-gray-800 rounded-md shadow-sm"></canvas>
+                            })">
+                                <canvas x-ref="canvas-{{ $queue }}"
+                                    class="ring-1 ring-gray-900/5 dark:ring-gray-100/10 bg-gray-50 dark:bg-gray-800 rounded-md shadow-sm"></canvas>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
-        </div>
-    @endif
+                @endforeach
+            </div>
+        @endif
     </x-pulse::scroll>
 </x-pulse::card>
 
 @script
-<script>
-Alpine.data('queueSizeChart', (config) => ({
-    init() {
-        let chart = new Chart(
-            this.$refs.canvas,
-            {
-                type: 'line',
-                data: {
-                    labels: ['queue_size'],
-                    datasets: [
-                        {
-                            label: config.queue,
-                            borderColor: '#9333ea',
-                            data: config.readings,
+    <script>
+        window.charts = []
+        Alpine.data('queueSizeChart', (config) => ({
+            init() {
+                window.charts.push(
+                    new Chart(
+                        this.$refs[`canvas-${config.queue}`], {
+                            type: 'line',
+                            data: {
+                                labels: [config.queue],
+                                datasets: [{
+                                    label: config.queue,
+                                    borderColor: '#9333ea',
+                                    data: config.readings,
+                                }],
+                            },
+                            options: {
+                                maintainAspectRatio: false,
+                                layout: {
+                                    autoPadding: false,
+                                    padding: {
+                                        top: 1,
+                                    },
+                                },
+                                datasets: {
+                                    line: {
+                                        borderWidth: 2,
+                                        borderCapStyle: 'round',
+                                        pointHitRadius: 10,
+                                        pointStyle: false,
+                                        tension: 0.2,
+                                        spanGaps: false,
+                                        segment: {
+                                            borderColor: (ctx) => ctx.p0.raw === 0 && ctx.p1.raw === 0 ?
+                                                'transparent' : undefined,
+                                        }
+                                    }
+                                },
+                                scales: {
+                                    x: {
+                                        display: false,
+                                    },
+                                    y: {
+                                        display: false,
+                                        min: 0,
+                                        max: this.highest(config.readings),
+                                    },
+                                },
+                                plugins: {
+                                    legend: {
+                                        display: false,
+                                    },
+                                    tooltip: {
+                                        mode: 'index',
+                                        position: 'nearest',
+                                        intersect: false,
+                                        callbacks: {
+                                            beforeBody: (context) => context
+                                                .map(item => `${item.dataset.label}`)
+                                                .join(', '),
+                                            label: () => null,
+                                        },
+                                    },
+                                },
+                            },
                         }
-                    ],
-                },
-                options: {
-                    maintainAspectRatio: false,
-                    layout: {
-                        autoPadding: false,
-                        padding: {
-                            top: 1,
-                        },
-                    },
-                    datasets: {
-                        line: {
-                            borderWidth: 2,
-                            borderCapStyle: 'round',
-                            pointHitRadius: 10,
-                            pointStyle: false,
-                            tension: 0.2,
-                            spanGaps: false,
-                            segment: {
-                                borderColor: (ctx) => ctx.p0.raw === 0 && ctx.p1.raw === 0 ? 'transparent' : undefined,
-                            }
-                        }
-                    },
-                    scales: {
-                        x: {
-                            display: false,
-                        },
-                        y: {
-                            display: true,
-                            min: 0,
-                            max: this.highest(config.queue),
-                        },
-                    },
-                    plugins: {
-                        legend: {
-                            display: false,
-                        },
-                    },
-                },
-            }
-        )
+                    ))
 
-        Livewire.on('queues-sizes-chart-update', ({ queues }) => {
-            if (chart === undefined) {
-                return
-            }
+                Livewire.on('queues-sizes-chart-update', ({
+                    queues
+                }) => {
+                    console.log(queues);
+                    let q = Object.keys(queues)[0];
 
-            let data = queues['default']['queue_size']
+                    let chart = window.charts.filter((c) => c.canvas.getAttribute('x-ref').includes(q))[
+                        0]
 
-            chart.data.labels = ['queue_size']
-            Object.keys(data).map(function (k) {
-                if(!data[k]){
-                    data[k] = 0;
-                } else {
-                    data[k] = parseInt(data[k])
-                }
-            } );
-            
-            chart.options.scales.y.max = this.highest(data)
-            chart.data.datasets[0].data = data
-            chart.update()
-        })
-    },
-    highest(readings) {
-        return Math.max(Object.values(readings));
-    },
-}))
-</script>
+                    if (chart === undefined) {
+                        return
+                    }
+
+                    chart.data.datasets[0].data = queues[q]
+                    chart.options.scales.y.max = this.highest(queues[q])
+                    chart.update()
+                })
+            },
+            highest(values) {
+                return Math.max(...Object.values(values)) + 1
+            },
+        }))
+    </script>
 @endscript
--- src/Http/Livewire/QueueSize.php
+++ src/Http/Livewire/QueueSize.php
@@ -4,25 +4,37 @@ namespace Biigle\PulseQueueSizeCard\Http\Livewire;
 
 use Livewire\Livewire;
 use Laravel\Pulse\Livewire\Card;
+use Illuminate\Support\Facades\Cache;
 
 // #[Lazy]
 class QueueSize extends Card
 {
     public function render()
     {
-        [$queues, $time, $runAt] = $this->remember(fn () => 
-            $this->graph(['queue_size'],'sum')
+
+        $types = Cache::get('queue_size', []);
+
+        [$queues, $time, $runAt] = $this->remember(
+            fn() =>
+            $this->graph($types, 'sum')
         );
 
+        if (sizeof($queues)) {
+            $queues = $queues['queue_size']->map(fn($v, $k) => $v->map(fn($vv, $kk) => intval($vv)));
+        } else {
+            $queues = collect([]);
+        }
+
         if (Livewire::isLivewireRequest()) {
-            $this->dispatch('queues-sizes-chart-update', queues: $queues);
+            foreach ($queues->keys() as $key => $value) {
+                $this->dispatch('queues-sizes-chart-update', queues: [$value => $queues[$value]]);
+            }
         }
 
         return view('pulse-queue-size-card::queue-size', [
             'queues' => $queues,
             'time' => $time,
             'runAt' => $runAt,
-
         ]);
     }
 }
