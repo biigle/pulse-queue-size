95f78a2ef1b496696c65facd99cedac70357206c
Leane Schlundt <lhecht@techfak.de>
2025-10-07 07:54:41 +0200

    Publish config when pulse starts

--- src/PulseQueueSizeCardServiceProvider.php
+++ src/PulseQueueSizeCardServiceProvider.php
@@ -27,5 +27,6 @@ class PulseQueueSizeCardServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', "pulse-queue-size-card");
 
+        $this->publishes([__DIR__.'/config/pulse-ext.php' => config_path('pulse-ext.php')]);
     }
 }
