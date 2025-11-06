60446029413cac54094c6c339dca24bf3cf39eba
Leane Schlundt <lhecht@techfak.de>
2025-11-06 09:27:13 +0100

    Ensure only a single machine uses the recoder

--- src/Recorders/QueueSize.php
+++ src/Recorders/QueueSize.php
@@ -5,12 +5,19 @@ namespace Biigle\PulseQueueSizeCard\Recorders;
 
 use Illuminate\Support\Facades\DB;
 use Laravel\Pulse\Events\SharedBeat;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Artisan;
-use Laravel\Pulse\Recorders\Concerns\Throttling;
+
 
 class QueueSize
 {
-    use Throttling;
+
+    /**
+     * Key to save update timestamps in cache
+     *
+     * @var string
+     */
+    protected string $LAST_UPDATE_KEY = 'queue_size_updated_at';
 
     /**
      * The events to listen for.
@@ -24,35 +31,44 @@ class QueueSize
     /**
      * Record the job.
      */
-    public function record(SharedBeat $event): void
+    public function record(): void
     {
-        // Default: 60 seconds
-        $interval = config('pulse-ext.record_interval');
+        $lock = Cache::lock('queue_size', 60);
 
-        // Record queue sizes
-        $this->throttle($interval, $event, function () {
-            $status = config('pulse-ext.queue_status');
-            $queues = config('pulse-ext.queues');
-            $defaultConnection = config('queue.default');
+        // Ensures only a single machine is executing the code
+        if ($lock->get()) {
+            // Default: 60 seconds
+            $interval = 10;
+            $lastUpdate = Cache::get($this->LAST_UPDATE_KEY);
 
-            foreach ($queues as $queue) {
-                if (!str_contains($queue, ":")) {
-                    $queue = "$defaultConnection:$queue";
+            // Record queue sizes
+            if ($lastUpdate === null || $lastUpdate->addSeconds($interval)->isNowOrPast()) {
+                Cache::put($this->LAST_UPDATE_KEY, now());
+                $status = config('pulse-ext.queue_status');
+                $queues = config('pulse-ext.queues');
+                $defaultConnection = config('queue.default');
+
+                foreach ($queues as $queue) {
+                    if (!str_contains($queue, ":")) {
+                        $queue = "$defaultConnection:$queue";
+                    }
+
+                    Artisan::call("queue:monitor $queue --json");
+                    $output = json_decode(Artisan::output(), true)[0];
+
+                    $values = [];
+                    foreach ($status as $state) {
+                        $values[$state] = $output[$state];
+                    }
+
+                    DB::table('queue_sizes')->insert([
+                        'queue' => $queue,
+                        'values' => json_encode($values),
+                    ]);
                 }
-
-                Artisan::call("queue:monitor $queue --json");
-                $output = json_decode(Artisan::output(), true)[0];
-
-                $values = [];
-                foreach ($status as $state) {
-                    $values[$state] = $output[$state];
-                }
-
-                DB::table('queue_sizes')->insert([
-                    'queue' => $queue,
-                    'values' => json_encode($values),
-                ]);
             }
-        });
+
+            $lock->release();
+        }
     }
 }
