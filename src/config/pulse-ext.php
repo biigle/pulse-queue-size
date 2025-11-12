9574c9365fc83dbe02b72f0bce9a85d50903bd9d
Leane Schlundt <lhecht@techfak.de>
2025-11-12 08:09:03 +0100

    Add comment

--- src/config/pulse-ext.php
+++ src/config/pulse-ext.php
@@ -4,6 +4,7 @@ return [
 
     'record_interval' => 60,
 
+    // Must be a key from the queue:monitor output pointing to an integer
     'queue_status' => ['pending', 'delayed', 'reserved'],
 
     'queue_size_card_id' => 'queue_size',
