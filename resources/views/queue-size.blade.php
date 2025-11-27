0aa3b5a0e8a70a4987c0e5bebb07c2e818df01e5
Martin Zurowietz <martin@zurowietz.de>
2025-11-27 10:26:37 +0100

    Fix position of graph key

--- resources/views/queue-size.blade.php
+++ resources/views/queue-size.blade.php
@@ -6,22 +6,24 @@
         <x-slot:icon>
             <x-pulse::icons.queue-list />
         </x-slot:icon>
+        <x-slot:actions>
+            <div class="flex flex-wrap gap-4">
+                <div class="flex items-center gap-2 text-xs text-gray-600 dark:text-gray-400 font-medium">
+                    <div class="h-0.5 w-3 rounded-full bg-[#e11d48]"></div>
+                    Pending
+                </div>
+                <div class="flex items-center gap-2 text-xs text-gray-600 dark:text-gray-400 font-medium">
+                    <div class="h-0.5 w-3 rounded-full bg-[#eab308]"></div>
+                    Delayed
+                </div>
+                <div class="flex items-center gap-2 text-xs text-gray-600 dark:text-gray-400 font-medium">
+                    <div class="h-0.5 w-3 rounded-full bg-[#9333ea]"></div>
+                    Reserved
+                </div>
+            </div>
+        </x-slot:actions>
     </x-pulse::card-header>
 
-    <div class="flex flex-wrap gap-4 mb-3">
-        <div class="flex items-center gap-2 text-xs text-gray-600 dark:text-gray-400 font-medium">
-            <div class="h-0.5 w-3 rounded-full bg-[#e11d48]"></div>
-            Pending
-        </div>
-        <div class="flex items-center gap-2 text-xs text-gray-600 dark:text-gray-400 font-medium">
-            <div class="h-0.5 w-3 rounded-full bg-[#eab308]"></div>
-            Delayed
-        </div>
-        <div class="flex items-center gap-2 text-xs text-gray-600 dark:text-gray-400 font-medium">
-            <div class="h-0.5 w-3 rounded-full bg-[#9333ea]"></div>
-            Reserved
-        </div>
-    </div>
 
     <x-pulse::scroll :expand="$expand" wire:poll.5s="">
         @if ($queues->isEmpty())
