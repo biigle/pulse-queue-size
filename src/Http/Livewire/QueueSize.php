4a8e1fb95d2c23e8dfcb6b75a8b6ec150794433a
Martin Zurowietz <martin@zurowietz.de>
2025-11-27 10:21:35 +0100

    Implement sorting of queue graphs and hiding empty ones

--- src/Http/Livewire/QueueSize.php
+++ src/Http/Livewire/QueueSize.php
@@ -2,9 +2,10 @@
 
 namespace Biigle\PulseQueueSizeCard\Http\Livewire;
 
-use Livewire\Livewire;
+use Biigle\PulseQueueSizeCard\Recorders\QueueSize as Recorder;
 use Illuminate\Support\Str;
 use Laravel\Pulse\Livewire\Card;
+use Livewire\Livewire;
 
 #[Lazy]
 class QueueSize extends Card
@@ -15,15 +16,31 @@ class QueueSize extends Card
             fn() => $this->graph(['pending', 'delayed', 'reserved'], 'max')
         );
 
+        // Show queues in the same order than they are defined in the config.
+        $sortIndex = array_flip(Recorder::getQueuesToRecord());
+        $queues = $queues->sortKeysUsing(
+            fn ($a, $b) => ($sortIndex[$a] ?? INF) - ($sortIndex[$b] ?? INF)
+        );
+
+        // Show only queues with values.
+        $queues = $queues->filter(function ($q) {
+            return $q['pending']->contains(fn ($v) => floatval($v) !== 0.0) ||
+                $q['delayed']->contains(fn ($v) => floatval($v) !== 0.0) ||
+                $q['reserved']->contains(fn ($v) => floatval($v) !== 0.0);
+        });
+
         if (Livewire::isLivewireRequest()) {
             $this->dispatch('queues-sizes-chart-update', queues: $queues);
         }
+
+        $defaultConnection = config('queue.default');
+        $showConnection = $queues->keys()->contains(fn ($q) => !str_starts_with($q, $defaultConnection.':'));
 
         return view('pulse-queue-size-card::queue-size', [
             'queues' => $queues,
             'time' => $time,
             'runAt' => $runAt,
-            'showConnection' => $queues->keys()->map(fn($queue) => Str::before($queue, ':'))->unique()->count() > 1,
+            'showConnection' => $showConnection,
         ]);
     }
 }
